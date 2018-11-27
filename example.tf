70d8a614eab9dd74b0a9ca78ccc44c7dc0479928
Nicholas Khozhylin <nik.khozhylin@gmail.com>
2018-11-27 14:52:01 +0200

    added variables to map

--- example.tf
+++ example.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0b0a60c0a2bd40612"
+  ami           = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
 
   depends_on = ["aws_s3_bucket.example"]
--- variables.tf
+++ variables.tf
@@ -1,5 +1,14 @@
 variable "access_key" {}
 variable "secret_key" {}
+variable "cidrs" { type = "list" }
 variable "region" {
   default = "eu-west-2"
+}
+
+variable "amis" {
+  type = "map"
+  default = {
+    "eu-central-1" = "ami-086a09d5b9fa35dc7"
+    "eu-west-2" = "ami-061a2d878e5754b62"
+  }
 }
